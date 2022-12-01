a06c01639531ead1b387456bf31ccffefc5bcebc
Álvaro <alvaro.marcos@factorlibre.com>
2022-12-01 15:59:28 +0100

    Realizar write al finalizar cálculo

--- product_compute_order_pvi/model/computed_purchase_order_line.py
+++ product_compute_order_pvi/model/computed_purchase_order_line.py
@@ -20,24 +20,28 @@ class ComputedPurchaseOrderLine(models.Model):
 
     @api.multi
     def _pvi_qty_available(self):
+        cpol_lines = {}
         for cpol in self:
             if cpol.product_id.id:
                 product_id = cpol.change_product_context(cpol.product_id)
                 pvi = [True]
                 parametres = ['draft', 'sent']
+                line_dict = {}
                 if cpol.computed_purchase_order_id.compute_pvi_d_quantity:
-                    cpol.pvi_draft_qty = product_id.\
+                    line_dict['pvi_draft_qty'] = product_id.\
                         custom_average_consumption(parametres, pvi)[0]
                 if cpol.computed_purchase_order_id.compute_pvi_quantity:
                     parametres = ['pvi_confirmed']
-                    cpol.pvi_qty = product_id.\
+                    line_dict['pvi_qty'] = product_id.\
                         custom_average_consumption(parametres, pvi)[0]
+                cpol_lines[cpol.id] = line_dict
+        return cpol_lines
 
     @job(default_channel='root.update_computed_qty')
     @api.multi
     def _get_computed_qty(self):
         """ Update computed purchase order quantities """
-        self._pvi_qty_available()
+        cpol_lines = self._pvi_qty_available() or {}
         self._product_qty_available()
         for cpol in self:
             computed_qty = 0
@@ -51,4 +55,13 @@ class ComputedPurchaseOrderLine(models.Model):
                 computed_qty -= cpol.pvi_draft_qty
             if cpol.computed_purchase_order_id.compute_pvi_quantity:
                 computed_qty -= cpol.pvi_qty
-            cpol.computed_qty = computed_qty
+            values = {
+                "computed_qty": computed_qty
+            }
+            pvi_value = cpol_lines.get(cpol.id)
+            if pvi_value:
+                if pvi_value.get("pvi_qty"):
+                    values['pvi_qty'] = pvi_value.get("pvi_qty")
+                if pvi_value.get("pvi_draft_qty"):
+                    values['pvi_draft_qty'] = pvi_value.get("pvi_draft_qty")
+            cpol.write(values)
